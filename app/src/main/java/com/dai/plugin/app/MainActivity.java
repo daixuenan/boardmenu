6b95ec7756fbdbcb829f6330b2ce1fba253848a4
daixuenan <1007573804@qq.com>
2019-04-24 16:51:21 +0800

    update

--- app/src/main/java/com/dai/plugin/app/MainActivity.java
+++ app/src/main/java/com/dai/plugin/app/MainActivity.java
@@ -5,16 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.dai.plugin.boardmenu.actions.Button10Action;
 import com.dai.plugin.boardmenu.actions.Button1Action;
 import com.dai.plugin.boardmenu.actions.Button2Action;
 import com.dai.plugin.boardmenu.actions.Button3Action;
-import com.dai.plugin.boardmenu.actions.Button4Action;
-import com.dai.plugin.boardmenu.actions.Button5Action;
-import com.dai.plugin.boardmenu.actions.Button6Action;
-import com.dai.plugin.boardmenu.actions.Button7Action;
-import com.dai.plugin.boardmenu.actions.Button8Action;
-import com.dai.plugin.boardmenu.actions.Button9Action;
 import com.dai.plugin.boardmenu.actions.base.BaseAction;
 import com.dai.plugin.boardmenu.widget.MenuPopView;
 
@@ -60,51 +53,6 @@ public class MainActivity extends AppCompatActivity {
         dataList.add(new Button3Action(this));
         dataList.add(new Button3Action(this));
         dataList.add(new Button3Action(this));
-        dataList.add(new Button3Action(this));
-        dataList.add(new Button3Action(this));
-        dataList.add(new Button3Action(this));
-
-        dataList.add(new Button4Action(this));
-        dataList.add(new Button4Action(this));
-        dataList.add(new Button4Action(this));
-        dataList.add(new Button4Action(this));
-        dataList.add(new Button4Action(this));
-        dataList.add(new Button4Action(this));
-        dataList.add(new Button4Action(this));
-        dataList.add(new Button4Action(this));
-
-        dataList.add(new Button5Action(this));
-        dataList.add(new Button5Action(this));
-        dataList.add(new Button5Action(this));
-        dataList.add(new Button5Action(this));
-        dataList.add(new Button5Action(this));
-        dataList.add(new Button5Action(this));
-        dataList.add(new Button5Action(this));
-        dataList.add(new Button5Action(this));
-
-        dataList.add(new Button6Action(this));
-        dataList.add(new Button6Action(this));
-        dataList.add(new Button6Action(this));
-        dataList.add(new Button6Action(this));
-        dataList.add(new Button6Action(this));
-        dataList.add(new Button6Action(this));
-        dataList.add(new Button6Action(this));
-        dataList.add(new Button6Action(this));
-
-        dataList.add(new Button7Action(this));
-        dataList.add(new Button7Action(this));
-        dataList.add(new Button7Action(this));
-        dataList.add(new Button7Action(this));
-        dataList.add(new Button7Action(this));
-        dataList.add(new Button7Action(this));
-        dataList.add(new Button7Action(this));
-        dataList.add(new Button7Action(this));
-
-        dataList.add(new Button8Action(this));
-
-        dataList.add(new Button9Action(this));
-
-        dataList.add(new Button10Action(this));
 
         menuPopView.setData(dataList);
 
